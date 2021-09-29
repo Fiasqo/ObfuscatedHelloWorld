b12910bf0603e8e01e11cba866287880a41e9344
Artyom Solobay <43761130+Fiasqo@users.noreply.github.com>
2021-09-29 15:37:08 +0300

    0x746E6949473A2920, 0x120756407530

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ObfuscatedHelloWorld {
+internal static class Program {
+    private static void Main() {
+        const long letters = 0x726F6C6564574820;
+        var indices = 0x357620655410;
+        var helloWorld = string.Empty;
+
+        while (indices != 0x0) helloWorld += (char) ((letters >> (((int) (indices >>= 4) & 7) << 3)) & 0xFF);
+        Console.WriteLine(helloWorld);
+    }
+}
+}
